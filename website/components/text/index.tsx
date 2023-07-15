5151bcc7a092a22a4244e7a2bea2ef47ceb3c2d2
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-14 23:06:02 -0400

    quasarapp.xyz

---
+++ website/components/text/index.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from './style.module.css'
+import TextTransition, { presets } from "react-text-transition";
+
+const TEXTS = [
+    "boats",
+    "trains",
+    "planes",
+    "whatever",
+];
+
+const Text = () => {
+    const [index, setIndex] = React.useState(0);
+
+    React.useEffect(() => {
+        const intervalId = setInterval(() =>
+            setIndex(index => index + 1),
+            2000 // every 3 seconds
+        );
+        return () => clearTimeout(intervalId);
+    }, []);
+
+    return (
+        
+        <h1 className={styles.subheader}>
+            light nodes on
+            <TextTransition springConfig={presets.wobbly} inline style={{marginLeft: '4px'}}> 
+             {TEXTS[index % TEXTS.length]}
+            </TextTransition>
+        </h1>
+        
+    );
+};
+
+export default Text;
