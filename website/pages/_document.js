aa038de31f76eb753d6c7f7047212803f821e791
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-15 03:49:21 -0400

    update

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -11,6 +11,9 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <script defer data-domain="quasarapp.xyz" src="https://plausible.io/js/script.js"></script>
+          <link rel="icon" href="/icons/favicon.ico" />
+          <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
+          <meta name="description" content="the first celestia light node macOS app - start your node in 30 seconds" />
         </Head>
         <body>
           <Main />
