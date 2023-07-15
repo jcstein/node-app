8457575371e644a5d4dfd7c0e5bddbff6e996b58
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-07-14 23:21:49 -0400

    Create _document.js

---
+++ website/pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <script defer data-domain="quasarapp.xyz" src="https://plausible.io/js/script.js"></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
