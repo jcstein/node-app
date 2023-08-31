1f3effc098152ce02d8e8d51c45ef9a8bcfed67e
joshcs.eth ᵍᵐ <mail@jcstein.dev>
2023-08-31 15:42:50 -0600

    swap snippet

--- website/pages/_document.js
+++ website/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <script defer data-domain="quasarapp.xyz" src="https://plausible.io/js/script.js"></script>
+          <script defer data-domain="quazarapp.xyz" src="https://plausible.io/js/script.js"></script>
           <link rel="icon" href="/icons/favicon.ico" />
           <link rel="icon" href="/icons/favicon.svg" type="image/svg+xml" />
           <meta name="description" content="the first celestia light node macOS app - start your node in 30 seconds" />
